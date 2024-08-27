5441a21cc0b2e9484d215560c09b083114f92303
moses kibii <mosesgodfriy@gmail.com>
2024-08-27 22:01:33 +0300

    answers

---
+++ user_input.py
@@ -0,0 +1,16 @@
+
+
+# Ask the user for their name and store it in the variable 'name'
+name = input("Please enter your name:")
+
+# Ask the user for their age and store it in the variable 'age'
+age = input("Please enter your age: ")
+
+# Ask the user for their location and store it in the variable 'location'
+location = input("Please enter your location: ")
+
+# Print out a personalized message using the collected inputs
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
+
+
